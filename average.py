dbeb987c703e8cfb44f78017117ba384fe022f00
Allen Jude <allenjudendk@gmail.com>
2025-08-13 21:39:00 -0700

    a liitle code to find the average of 3 numbers

---
+++ average.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the seccond number: "))
+num3 = int(input("Enter the third number: "))
+print("The average of the 3 numbers is", (num1 + num2 + num3) / 3)
