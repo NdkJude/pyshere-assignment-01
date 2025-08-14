26a546f05939ca26d126f30138c09fd6e17713b1
Allen Jude <allenjudendk@gmail.com>
2025-08-13 21:48:00 -0700

    just added a little code to handle average of 3 numbers

--- average.py
+++ average.py
@@ -2,3 +2,9 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the seccond number: "))
 num3 = int(input("Enter the third number: "))
 print("The average of the 3 numbers is", (num1 + num2 + num3) / 3)
+
+num4 = int(input("Enter any number of your choice: "))
+if num4 >= 18:
+    print("You are an adult")
+else:
+    print("You are a underage")
